2b5ecb47636e98f5d7544313cc8aa23c3ce4c35d
svallapu515 <svallapu515@gmail.com>
2025-03-17 12:57:49 -0400

    first push

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  standalone: true,
+  imports: [],
+  templateUrl: './sample.component.html',
+  styleUrl: './sample.component.css'
+})
+export class SampleComponent {
+
+}
